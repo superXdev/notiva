f7b3ebea10a40e3eaeb1bab43dbb7986ad186b4b
Fikri <fikrizhanking@gmail.com>
2025-09-05 19:22:09 +0700

    config: opennextjs for cloudflare pages

---
+++ .open-next/assets/_next/static/chunks/pages/_error-cac7e7a749488614.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{44975:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(56959)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(44975)),_N_E=_.O()}]);
--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
-export const runtime = "edge";
-
 export async function GET(request: NextRequest) {
    const { searchParams } = new URL(request.url);
    const title = searchParams.get("title") || "Untitled Note";
